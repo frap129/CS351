ff7fead524127a4aac8ab5d6b99607cbaae35922
Joe Maples <joe@maples.dev>
2020-05-06 19:22:13 -0400

    Add Homework 2

---
+++ hw2/client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+import socket
+
+# Configurables
+filePath = "example.txt"
+serverIP = "54.235.47.119"
+
+# Argument Parsing
+if (len(sys.argv) >= 2):
+    filePath = sys.argv[1]
+if (len(sys.argv) == 3):
+    serverIP = sys.argv[2]
+
+# Constants
+serverPort = 351 # CS-351, to be exact
+fileSizeLimit = 1024
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to server
+print("Connecting to " + serverIP + ":" + serverPort.__str__())
+clientSocket.connect((serverIP, serverPort))
+print("Connected!")
+
+# Read File
+print("Sending file...")
+file = open(filePath, 'rb')
+fileBuffer = file.read(fileSizeLimit)
+
+# Send File
+clientSocket.send(fileBuffer)
+file.close()
+
+# Close connection
+print("Closing connection")
+clientSocket.shutdown(socket.SHUT_WR)
+clientSocket.close()
---
+++ hw2/server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import socket
+
+# Constants
+fileSizeLimit = 1024
+serverSocket = socket.socket()
+serverPort = 351
+
+# Configurables
+serverIP = "172.31.28.97"
+
+# Bind to socket
+print("Binding to socket at " + serverIP + ":" + serverPort.__str__())
+serverSocket.bind((serverIP, serverPort))
+print("Successfully bound to socket")
+
+# Listen for incomming traffic
+print("Listening...")
+serverSocket.listen(10)
+
+# Receive file
+socket, address = serverSocket.accept()
+print("Connection from ", address)
+file = socket.recv(fileSizeLimit).decode('utf-8')
+print("File transfer complete")
+
+lineArr = file.splitlines()
+print("Lines:",len(lineArr))
+words = 0
+chars = 0
+for line in lineArr:
+    words += len(line.split())
+    chars += len(line.strip())
+print("Words:", words)
+print("Chars:", chars)
